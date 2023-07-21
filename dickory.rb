75889f1d0e5242d3de8f6684446f05e9f43754cb
Mike Hoyle <hoylemd@gmail.com>
2023-07-21 18:43:00 -0400

    add the demo files

---
+++ dickory.rb
@@ -0,0 +1,13 @@
+def say(message)
+  p "Dickory: #{message}"
+end
+
+say "I am Dickory!"
+say "We shall begin reciting in 5 seconds"
+sleep 5
+sleep 1
+say "The mouse ran up the clock"
+sleep 2
+say "The mouse ran down"
+sleep 10
+say "EXECLSIOR!!!"
---
+++ hickory.rb
@@ -0,0 +1,12 @@
+def say(message)
+  p "Hickory: #{message}"
+end
+
+say "I am Hickory!"
+say "I shall begin reciting in 5 seconds"
+sleep 5
+say "Hickory Dickory Dock"
+sleep 2
+say "The clock struck one"
+sleep 2
+say "Hickory Dickory Dock"
